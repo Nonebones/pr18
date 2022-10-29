a3ee39159f98527fdbe9c50bc6139961f88238f3
Golomraz <vitalik.klbd@gmail.com>
2022-10-29 10:08:12 +0300

    first commit

---
+++ app/src/main/java/com/example/pr18/SecondFragment.kt
@@ -0,0 +1,28 @@
+package com.example.pr18
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+class SecondFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    fun setSelectedItem(selectedItem: String?) {
+        val view = view?.findViewById<TextView>(R.id.textView)
+        view?.text = selectedItem
+    }
+}
